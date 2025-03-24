40613f22a6b6a383faf28954874d6e502678e60b
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-03-24 18:51:58 +0900

    [2025-03-24] feat: 메모 작성 및 수정 프래그먼트를 위한 액티비티 추가

---
+++ app/src/main/java/com/bossmg/android/memoir/MemoActivity.kt
@@ -0,0 +1,25 @@
+package com.bossmg.android.memoir
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.bossmg.android.memoir.databinding.ActivityMainBinding
+import com.bossmg.android.memoir.databinding.ActivityMemoBinding
+
+class MemoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMemoBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        if(savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.memo_activity_container, AddMemoFragment())
+                .commit()
+        }
+    }
+}
