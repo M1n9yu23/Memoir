50f9ac6178fb24200ce6a80151fd54d7e7eb1f4b
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-03-26 15:37:17 +0900

    [2025-03-26] feat: CalendarFragment
    리사이클러뷰의 홀더와 어댑터를 추상화하여 프래그먼트의 기능만 하도록 함.

--- app/src/main/java/com/bossmg/android/memoir/CalendarFragment.kt
+++ app/src/main/java/com/bossmg/android/memoir/CalendarFragment.kt
@@ -2,23 +2,27 @@ package com.bossmg.android.memoir
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.bossmg.android.memoir.MainActivity.Companion.memos
 import com.bossmg.android.memoir.databinding.FragmentCalendarBinding
-import com.bossmg.android.memoir.databinding.ItemMemoBinding
 import com.prolificinteractive.materialcalendarview.CalendarDay
 
+
+private const val TAG = "CalendarFragment"
 class CalendarFragment : Fragment() {
 
     private val eventDates = HashSet<CalendarDay>()
     private lateinit var memoAdapter: MemoAdapter
+    private lateinit var binding: FragmentCalendarBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate...")
     }
 
     override fun onCreateView(
@@ -26,23 +30,20 @@ class CalendarFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentCalendarBinding.inflate(inflater, container, false)
+        binding = FragmentCalendarBinding.inflate(inflater, container, false)
 
         memoAdapter = MemoAdapter(emptyList())
         binding.calendarRecyclerView.layoutManager = LinearLayoutManager(requireContext())
         binding.calendarRecyclerView.adapter = memoAdapter
 
-        val allMemos = MyApplication.db.getAllMemos()
-        for (memo in allMemos) {
+        for (memo in memos) {
             val dateParts = memo.date.split("-")
             val calendarDay = CalendarDay.from(dateParts[0].toInt(), dateParts[1].toInt(), dateParts[2].toInt())
             eventDates.add(calendarDay)
         }
 
-        // 점 색상 설정
-        val dotColor = Color.CYAN
         // 캘린더뷰에 데코레이터 추가
-        binding.calendarView.addDecorator(EventDecorator(dotColor, eventDates))
+        binding.calendarView.addDecorator(EventDecorator(Color.CYAN, eventDates))
 
         // 날짜 클릭 리스너 추가
         binding.calendarView.setOnDateChangedListener { widget, date, selected ->
@@ -60,36 +61,15 @@ class CalendarFragment : Fragment() {
         return binding.root
     }
 
-    private inner class MemoViewHolder(val binding: ItemMemoBinding) :
-        RecyclerView.ViewHolder(binding.root)
-
-    private inner class MemoAdapter(private var memoList: List<MemoItem>) :
-        RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-
-        fun updateMemos(newMemoList: List<MemoItem>) {
-            memoList = newMemoList
-            notifyDataSetChanged() // 데이터가 변경되었음을 알리고 뷰를 새로 고침
-        }
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
-            MemoViewHolder(
-                ItemMemoBinding.inflate(
-                    LayoutInflater.from(parent.context), parent, false
-                )
-            )
-
-        override fun getItemCount(): Int = memoList.size
-
-        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-            val binding = (holder as MemoViewHolder).binding
-
-            val memo = memoList[position]
-
-            binding.memoTitle.text = memo.title
-            binding.memoDateNumber.text = memo.dateNumber
-            binding.memoDate.text = "${memo.date} (${memo.dayOfWeek})"
-            binding.memoDay.text = memo.dayOfWeek
-            binding.memoMainImage.setImageBitmap(memo.image)
+    override fun onResume() {
+        super.onResume()
+        if (MyApplication.db.getAllMemos() != memos) {
+            for (memo in memos) {
+                val dateParts = memo.date.split("-")
+                val calendarDay = CalendarDay.from(dateParts[0].toInt(), dateParts[1].toInt(), dateParts[2].toInt())
+                eventDates.add(calendarDay)
+            }
+            binding.calendarView.invalidateDecorators()
         }
     }
 }
