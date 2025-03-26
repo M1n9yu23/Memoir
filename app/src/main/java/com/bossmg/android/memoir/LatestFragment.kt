330d710781189fe0763751931cf2a4ae1416cfaf
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-03-26 15:30:19 +0900

    [2025-03-26] chore: LatestFragment
    리사이클러뷰의 홀더와 어댑터를 추상화하여 프래그먼트의 기능만 하도록 함.
    
    그리고 resume 생명주기함수에서 데이터 갱신을 계속해서 성능적으로 보완하기 위해 새로고침 기능을 넣음.

--- app/src/main/java/com/bossmg/android/memoir/LatestFragment.kt
+++ app/src/main/java/com/bossmg/android/memoir/LatestFragment.kt
@@ -6,20 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.bossmg.android.memoir.MainActivity.Companion.memos
+import com.bossmg.android.memoir.MainActivity.Companion.updateMemos
 import com.bossmg.android.memoir.databinding.FragmentLatestBinding
-import com.bossmg.android.memoir.databinding.ItemMemoBinding
+
+private const val TAG = "LatestFragment"
 
 class LatestFragment : Fragment() {
 
-    private lateinit var memos: List<MemoItem>
     private lateinit var memoAdapter: MemoAdapter
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        memos = MyApplication.db.getAllMemos()
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,49 +27,15 @@ class LatestFragment : Fragment() {
         binding.latestRecyclerView.layoutManager = LinearLayoutManager(requireContext())
         binding.latestRecyclerView.adapter = memoAdapter
 
+        binding.latestRefresh.setOnRefreshListener {
+            memoAdapter.updateMemos(updateMemos)
+            binding.latestRefresh.isRefreshing = false
+        }
+        
         return binding.root
-    }
-
-    private inner class MemoViewHolder(val binding: ItemMemoBinding) :
-        RecyclerView.ViewHolder(binding.root)
-
-    private inner class MemoAdapter(private var memoList: List<MemoItem>) :
-        RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
-            MemoViewHolder(
-                ItemMemoBinding.inflate(
-                    LayoutInflater.from(parent.context), parent, false
-                )
-            )
-
-        override fun getItemCount(): Int = memoList.size
-
-        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-            val binding = (holder as MemoViewHolder).binding
-
-            val memo = memoList[position]
-
-            binding.memoTitle.text = memo.title
-            binding.memoDateNumber.text = memo.dateNumber
-            binding.memoDate.text = memo.date
-            binding.memoDay.text = memo.dayOfWeek
-            binding.memoMainImage.setImageBitmap(memo.image)
-        }
-
-        fun updateMemos(newMemoList: List<MemoItem>) {
-            memoList = newMemoList
-            notifyDataSetChanged()
-        }
     }
 
     override fun onResume() {
         super.onResume()
-        // db 갱신
-        val updateMemos = MyApplication.db.getAllMemos()
-        if (updateMemos != memos) {
-            memos = updateMemos
-            memoAdapter.updateMemos(memos)
-        }
     }
 }
