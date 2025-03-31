bf50dfee739e219b7dfd1c9603ec4388075ede48
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-03-31 15:47:27 +0900

    [2025-03-31] chore: EventDecorator
    캘린더 도트 크기 수정

--- app/src/main/java/com/bossmg/android/memoir/decorator/EventDecorator.kt
+++ app/src/main/java/com/bossmg/android/memoir/decorator/EventDecorator.kt
@@ -5,15 +5,14 @@ import com.prolificinteractive.materialcalendarview.DayViewDecorator
 import com.prolificinteractive.materialcalendarview.DayViewFacade
 import com.prolificinteractive.materialcalendarview.spans.DotSpan
 
-class EventDecorator(
-    private val color: Int,
-    private val dates: HashSet<CalendarDay>
-): DayViewDecorator {
-    override fun shouldDecorate(day: CalendarDay): Boolean {
-        return dates.contains(day)
+class EventDecorator(private val color: Int, private val eventDates: HashSet<CalendarDay>) :
+    DayViewDecorator {
+
+    override fun shouldDecorate(day: CalendarDay?): Boolean {
+        return eventDates.contains(day)
     }
 
-    override fun decorate(view: DayViewFacade) {
-        view.addSpan(DotSpan(10f, color))
+    override fun decorate(view: DayViewFacade?) {
+        view?.addSpan(DotSpan(8f, color))
     }
 }
