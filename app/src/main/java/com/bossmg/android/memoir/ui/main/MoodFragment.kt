16f903d59664e59294c126f5fd04f913fb3ad2ef
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-03-31 15:53:49 +0900

    [2025-03-31] feat: fragment_mood.xml, MoodFragment
    이모지를 클릭하면 해당 이모즈의 기록들이 나오도록 함.

--- app/src/main/java/com/bossmg/android/memoir/ui/main/MoodFragment.kt
+++ app/src/main/java/com/bossmg/android/memoir/ui/main/MoodFragment.kt
@@ -7,9 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bossmg.android.memoir.data.model.MemoItem
 import com.bossmg.android.memoir.R
+import com.bossmg.android.memoir.adapter.recycler.MemoAdapter
 import com.bossmg.android.memoir.databinding.FragmentMoodBinding
 import com.bossmg.android.memoir.databinding.ItemMoodBinding
 import com.bossmg.android.memoir.ui.main.MainActivity.Companion.memos
@@ -17,6 +19,10 @@ import com.bossmg.android.memoir.ui.main.MainActivity.Companion.memos
 private const val TAG = "MoodFragment"
 
 class MoodFragment : Fragment() {
+
+    private lateinit var moodAdapter: MoodAdapter
+    private lateinit var memoAdapter: MemoAdapter
+    private var binding: FragmentMoodBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,26 +34,40 @@ class MoodFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentMoodBinding.inflate(inflater, container, false)
+        binding = FragmentMoodBinding.inflate(inflater, container, false)!!
 
-        // 이모지 목록 가져오기
-        val emojiList = getMoodEmojiList()
+        moodAdapter = MoodAdapter(changeMemo(memos))
+        memoAdapter = MemoAdapter(emptyList())
 
-        // 이모지 중복 횟수 계산
-        val moodCountMap = getMoodCount(memos, emojiList)
-        val moodList = moodCountMap.map { (emoji, count) -> MoodItem(emoji, count) }
+        binding!!.moodRecycler.layoutManager = GridLayoutManager(requireContext(), 3)
+        binding!!.moodRecycler.adapter = moodAdapter
+        binding!!.memoMoodRecycler.layoutManager = LinearLayoutManager(requireContext())
+        binding!!.memoMoodRecycler.adapter = memoAdapter
 
-        binding.moodRecycler.layoutManager = GridLayoutManager(requireContext(), 3)
-        binding.moodRecycler.adapter = MoodAdapter(moodList)
+        return binding!!.root
+    }
 
-        return binding.root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        moodAdapter.onMoodClickListener = { emoji ->
+            val filterMoodMemos = memos.filter { it.mood.startsWith(emoji)} // emoji로 시작하는 것을 필터
+            memoAdapter.updateMemos(filterMoodMemos)
+        }
     }
 
     private inner class MoodViewHolder(val binding: ItemMoodBinding) :
         RecyclerView.ViewHolder(binding.root)
 
-    private inner class MoodAdapter(private val moods: List<MoodItem>) :
+    private inner class MoodAdapter(private var moods: List<MoodItem>) :
         RecyclerView.Adapter<MoodViewHolder>() {
+
+            var onMoodClickListener: ((String) -> Unit)? = null
+
+        fun updateMoods(newMoods: List<MoodItem>) {
+            moods = newMoods
+            notifyDataSetChanged()
+        }
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MoodViewHolder =
             MoodViewHolder(
@@ -63,6 +83,13 @@ class MoodFragment : Fragment() {
             val mood = moods[position]
             binding.moodEmoji.text = mood.emoji
             binding.moodNumber.text = mood.count.toString()
+
+            binding.moodEmoji.setOnClickListener { view ->
+                onMoodClickListener?.let {
+                    it(mood.emoji)
+                } // 수신자 객체가 람다로 전달되면서 위에 정의한 함수를 실행
+            }
+
         }
     }
 
@@ -87,5 +114,29 @@ class MoodFragment : Fragment() {
         }
 
         return moodCountMap
+    }
+
+    private fun changeMemo(memoList: List<MemoItem>): List<MoodItem> {
+        // 이모지 목록 가져오기
+        val emojiList = getMoodEmojiList()
+
+        // 이모지 중복 횟수 계산
+        val moodCountMap = getMoodCount(memoList, emojiList)
+        val moodList = moodCountMap.map { (emoji, count) -> MoodItem(emoji, count) }
+
+        return moodList
+    }
+
+    override fun onStart() {
+        super.onStart()
+        moodAdapter.updateMoods(changeMemo(memos))
+        Log.d(TAG, "onStart...")
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d(TAG, "onDestroyView...")
+        binding = null
     }
 }
