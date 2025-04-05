8a733d5fa3f9c66cb5cd4ff1a16b7a1a9c3aab69
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-04-05 20:23:39 +0900

    [2025-03-31] chore: LatestFragment

--- app/src/main/java/com/bossmg/android/memoir/ui/main/LatestFragment.kt
+++ app/src/main/java/com/bossmg/android/memoir/ui/main/LatestFragment.kt
@@ -1,6 +1,7 @@
 package com.bossmg.android.memoir.ui.main
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,34 +10,62 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.bossmg.android.memoir.adapter.recycler.MemoAdapter
 import com.bossmg.android.memoir.databinding.FragmentLatestBinding
 import com.bossmg.android.memoir.ui.main.MainActivity.Companion.memos
-import com.bossmg.android.memoir.ui.main.MainActivity.Companion.updateMemos
 
 private const val TAG = "LatestFragment"
 
 class LatestFragment : Fragment() {
 
     private lateinit var memoAdapter: MemoAdapter
+    private var binding: FragmentLatestBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentLatestBinding.inflate(inflater, container, false)
+        binding = FragmentLatestBinding.inflate(inflater, container, false)
 
         memoAdapter = MemoAdapter(memos)
-        binding.latestRecyclerView.layoutManager = LinearLayoutManager(requireContext())
-        binding.latestRecyclerView.adapter = memoAdapter
+        binding!!.latestRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding!!.latestRecyclerView.adapter = memoAdapter
 
-        binding.latestRefresh.setOnRefreshListener {
-            memoAdapter.updateMemos(updateMemos)
-            binding.latestRefresh.isRefreshing = false
+        binding!!.latestRefresh.setOnRefreshListener {
+            memoAdapter.updateMemos(memos)
+            binding!!.latestRefresh.isRefreshing = false
         }
 
-        return binding.root
+        return binding!!.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d(TAG, "onViewCreated")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart...")
     }
 
     override fun onResume() {
         super.onResume()
+        Log.d(TAG, "onResume...")
     }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause...")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop...")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d(TAG, "onDestroyView...")
+        binding = null
+    }
+
 }
